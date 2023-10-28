689033249edf5f889df050e95f99f29c4f97a2fd
Josué Yovani <josue.yovani@outlook.com>
2023-10-27 18:04:09 -0600

    Feat conditional render of the cover_img of episode image

--- src/components/Episode/Episode.js
+++ src/components/Episode/Episode.js
@@ -3,7 +3,15 @@ import React from "react";
 export default function Episode({ name, air_date, episode, cover_img }) {
   return (
     <div className="episode">
-      <img src={cover_img} alt={name} width={300} />
+      {cover_img ? (
+        <img src={cover_img} alt={name} width={300} />
+      ) : (
+        <img
+          src="https://enteracloud.mx/wp-content/uploads/2021/08/placeholder.png"
+          alt={name}
+          width={300}
+        />
+      )}
       <h1>{name}</h1>
       <h2>{air_date}</h2>
       <h3>{episode}</h3>
